4a14678661c617fdcdf57656db3e2fc3691bed94
CorbinBlackard <59624967+CorbinBlackard@users.noreply.github.com>
2025-11-24 18:30:10 -0500

    adding notes redirects back to same page you added on

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,48 +1,61 @@
 class NotesController < ApplicationController
-  def create
-    @book = Book.find(params[:book_id])
-    @note = @book.notes.build(note_params)
-    @note.user = current_user
+before_action :set_book
+before_action :set_note, only: [ :edit, :update, :destroy ]
 
-    if @note.save
+# POST /books/:book_id/notes
+def create
+   @note = @book.notes.build(note_params)
+   @note.user = current_user
+
+   if @note.save
       respond_to do |format|
-        format.turbo_stream
-        format.html { redirect_to book_path(@book), notice: "Note added successfully." }
+      format.turbo_stream
+      format.html { redirect_back(fallback_location: book_path(@book), notice: "Note added successfully.") }
       end
-    else
+   else
       respond_to do |format|
-        format.turbo_stream { render turbo_stream: turbo_stream.replace("note_form", partial: "notes/form", locals: { book: @book, note: @note }) }
-        format.html { redirect_to book_path(@book), alert: "Cannot add empty note." }
+      format.turbo_stream do
+         render turbo_stream: turbo_stream.replace(
+            "note_form",
+            partial: "notes/form",
+            locals: { book: @book, note: @note }
+         )
       end
-    end
-  end
+      format.html { redirect_back(fallback_location: book_path(@book), alert: "Cannot add empty note.") }
+      end
+   end
+end
 
-  def destroy
-    @book = Book.find(params[:book_id])
-    @note = @book.notes.find(params[:id])
-    @note.destroy
-    redirect_to root_path, notice: "Note deleted successfully."
-  end
+# GET /books/:book_id/notes/:id/edit
+def edit
+end
 
-  def edit
-    @book = Book.find(params[:book_id])
-    @note = @book.notes.find(params[:id])
-  end
-
-  def update
-    @book = Book.find(params[:book_id])
-    @note = @book.notes.find(params[:id])
-
-    if @note.update(note_params)
-      redirect_to root_path, notice: "Note updated successfully."
-    else
+# PATCH/PUT /books/:book_id/notes/:id
+def update
+   if @note.update(note_params)
+      redirect_back(fallback_location: book_path(@book), notice: "Note updated successfully.")
+   else
       render :edit
-    end
-  end
+   end
+end
+
+# DELETE /books/:book_id/notes/:id
+def destroy
+   @note.destroy
+   redirect_back(fallback_location: book_path(@book), notice: "Note deleted successfully.")
 end
 
 private
 
+def set_book
+   @book = Book.find(params[:book_id])
+end
+
+def set_note
+   @note = @book.notes.find(params[:id])
+end
+
 def note_params
-  params.require(:note).permit(:content)
+   params.require(:note).permit(:content)
+end
 end
--- app/models/note.rb
+++ app/models/note.rb
@@ -2,4 +2,6 @@ class Note < ApplicationRecord
   belongs_to :book
   belongs_to :user
   validates :content, presence: true
+  # Scope to get only notes with content
+  scope :with_content, -> { where.not(content: [ nil, "" ]) }
 end
