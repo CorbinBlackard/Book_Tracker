9a15c5196a141f9babc3f51e6983f84e03e76738
CorbinBlackard <59624967+CorbinBlackard@users.noreply.github.com>
2025-12-09 17:05:17 -0500

    added emojis in bookhelper

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -1,2 +1,16 @@
 module BooksHelper
+    def genre_emoji(genre)
+      case genre
+      when "Fantasy" then "🐦‍🔥🗡️🏞️"
+      when "Scifi" then "🛰️🧬🪐"
+      when "Historical Fiction" then "🏺📜"
+      when "Mystery" then "🕵️‍♂️🔍"
+      when "Thriller" then "🩸💀🔪"
+      when "Romance" then "❤️💌"
+      when "Horror" then "👻🩸"
+      when "Fiction" then "👤📖"
+      when "Self-Help" then "💡📝"
+      else "📘"
+      end
+    end
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,47 +1,47 @@
 class Book < ApplicationRecord
-    # Validations
-    validates :title, presence: true
-    validates :author, presence: true
-    validates :genre, presence: true
-    validates :page_number, numericality: { only_integer: true, greater_than_or_equal_to: 1 }, allow_nil: true
-    validates :rating, inclusion: { in: 1..5, message: "must be between 1 and 5" }, allow_nil: true
-    belongs_to :user
-    has_many :notes, dependent: :destroy
-    has_many :wishlists
-    has_many :users_wishlist, through: :wishlists, source: :user
+  # Validations
+  validates :title, presence: true
+  validates :author, presence: true
+  validates :genre, presence: true
+  validates :page_number, numericality: { only_integer: true, greater_than_or_equal_to: 1 }, allow_nil: true
+  validates :rating, inclusion: { in: 1..5, message: "must be between 1 and 5" }, allow_nil: true
+  belongs_to :user
+  has_many :notes, dependent: :destroy
+  has_many :wishlists
+  has_many :users_wishlist, through: :wishlists, source: :user
 
-    # Instance Methods
-    # Method to calculate the progress percentage
-    def progress_percentage
-        return 0 if total_pages == 0 and page_number == 0 # Prevent error if no page number inserted
+  # Instance Methods
+  # Method to calculate the progress percentage
+  def progress_percentage
+      return 0 if total_pages == 0 and page_number == 0 # Prevent error if no page number inserted
 
-        # Calculate and return the percentage of pages read
-        ((page_number.to_f / total_pages.to_f) * 100).round(2)
-    end
+      # Calculate and return the percentage of pages read
+      ((page_number.to_f / total_pages.to_f) * 100).round(2)
+  end
 
-    # app/models/book.rb
-    before_save :normalize_genre
+  # app/models/book.rb
+  before_save :normalize_genre
 
-    def normalize_genre
-      return if genre.blank?
-      self.genre = genre.strip.split.map(&:capitalize).join(" ")
-    end
+  def normalize_genre
+    return if genre.blank?
+    self.genre = genre.strip.split.map(&:capitalize).join(" ")
+  end
 
-    # Class Methods
-    # Method to count finished books (read: true)
-    def self.finished_books_count
-        where(read: true).count
-    end
+  # Class Methods
+  # Method to count finished books (read: true)
+  def self.finished_books_count
+      where(read: true).count
+  end
 
-    def self.currently_reading
-        where(currently_reading: true)
-    end
+  def self.currently_reading
+      where(currently_reading: true)
+  end
 
-    def self.currently_reading_count
-        where(currently_reading: true).count
-    end
+  def self.currently_reading_count
+      where(currently_reading: true).count
+  end
 
-    def self.favorite_books
-      where(favorite: true)
-    end
+  def self.favorite_books
+    where(favorite: true)
+  end
 end
